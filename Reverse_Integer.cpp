df3587746e290ca83b7d1d3d7b40cab39d688f8d
JingRyu <81526024+JingRyu@users.noreply.github.com>
2021-04-06 21:08:12 -0400

    verson1_not good
    verson1_not good

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -1,28 +1,81 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//consider int as string this time. will try another
 class Solution {
 public:
     int reverse(int x) {
         int single_digit;
         int remainer = x;
-        long long new_number = 0;
-        int counter = 0;
+        string new_raw_string;
 
-        while(x != 0){
-            x = x/10;
-            counter++;
-        }
-
-
-        while(remainer != 0)
+        //x<0
+        if(x>0){
+            while(remainer != 0)
             {
                 single_digit = remainer %10;
                 remainer = remainer/10;
 
-                new_number = new_number + single_digit*pow(10,counter-1);
-                counter--;
+                new_raw_string = new_raw_string + to_string(single_digit);
+
             }
 
-        if(new_number<INT_MIN || new_number>INT_MAX){return 0;}else{return  new_number;}
+             return  atoi(c_str(new_raw_string));
+        //x>0
+        }else if(x<0){
 
+            remainer = x * (-1);
+            while(remainer != 0)
+            {
+                single_digit = remainer %10;
+                remainer = remainer/10;
 
+                new_raw_string = new_raw_string + to_string(single_digit);
+            }
+
+                return -atoi(c_str(new_raw_string));
+        // x==0
+        }else{
+
+            return 0;
+        }
     }
 };
+
+
+int main()
+{
+    Solution test;
+    cout<<test.reverse(56799000)<<endl;
+    cout<<test.reverse(0)<<endl;
+    cout<<test.reverse(-121234)<<endl;
+    cout<<test.reverse(1534236469)<<endl;
+
+
+    //Solution(0);
+    //Solution(-121234);
+    //Solution(-123);
+
+    return 0;
+}
+
+
+/*int main()
+{
+    //Solution test;
+    //test.reverse(56799000);
+    int single_digit;
+        int remainer;
+        string new_raw_string = "1";
+        string y = "2";
+        string x = new_raw_string + y;
+        cout<<x;
+
+
+    //Solution(0);
+    //Solution(-121234);
+    //Solution(-123);
+
+    return 0;
+}*/
